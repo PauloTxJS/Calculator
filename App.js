1140a868b7ecbe2b2ddc4f9c091f63cc4486ffd0
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-08-07 11:53:25 -0300

    Começei a desenvolver a lógica da calculadora

--- App.js
+++ App.js
@@ -3,10 +3,44 @@ import { Platform, StyleSheet, Text, View } from 'react-native';
 import Button from './src/components/Button';
 import Display from './src/components/Display';
 
+const initialState = {
+	displayValue: '0',
+	clearDisplay: false,
+	operation: null,
+	values: [0, 0],
+	current: 0,
+}
+
 export default class App extends Component {
 
-	state = {
-		displayValue: '0'
+	state = { ...initialState }
+
+	addDigit = n => {
+
+		if (n === '.' && this.state.displayValue.includes('.')) {
+			return 
+		}
+
+		const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+		const currentValue = clearDisplay ? '' : this.state.displayValue
+		const displayValue = currentValue + n
+		this.setState({ displayValue, clearDisplay: false })
+		
+		if (n !== '.') {
+			const newValue = parseFloat(displayValue)
+			const values = [...this.state.values]
+			values[this.state.current] = newValue
+			this.setState({ values }) 	
+		}
+		
+	}
+
+	clearMemory = () => {
+		this.setState({ ...initialState }) 
+	}
+
+	setOperation = operation => {
+
 	}
 
 	render() {
@@ -14,23 +48,24 @@ export default class App extends Component {
 			<View style={styles.container}>
 				<Display value={this.state.displayValue} />
 				<View style={styles.button}>
-					<Button label='AC' />
-					<Button label='/' />	
-					<Button label='7' />
-					<Button label='8' />
-					<Button label='9' />
-					<Button label='*' />
-					<Button label='4' />
-					<Button label='5' />
-					<Button label='6' />
-					<Button label='-' />
-					<Button label='1' />
-					<Button label='2' />
-					<Button label='3' />
-					<Button label='+' />
-					<Button label='0' />
-					<Button label='.' />
-					<Button label='=' />
+					<Button label='AC' triple onClick={this.clearMemory}/>
+					<Button label='&#247;' operation onClick={() => this.setOperation('/')}/>	
+					<Button label='7' onClick={() => this.addDigit(7)}/>
+					<Button label='8' onClick={() => this.addDigit(8)}/>
+					<Button label='9' onClick={() => this.addDigit(9)}/>
+					<Button label='x' operation onCLick={() => this.setOperation('*')}/>
+					<Button label='4' onClick={() => this.addDigit(4)}/>
+					<Button label='5' onClick={() => this.addDigit(5)}/>
+					<Button label='6' onClick={() => this.addDigit(6)}/>
+					<Button label='-' operation onClick={() => this.setOperation('-')}/>
+					<Button label='1' onClick={() => this.addDigit(1)}/>
+					<Button label='2' onClick={() => this.addDigit(2)}/>
+					<Button label='3' onClick={() => this.addDigit(3)}/>
+					<Button label='+' operation onClick={() => this.setOperation('+')}/>
+					<Button label='0' onClick={() => this.addDigit(0)}/>
+					<Button label='.' onClick={() => this.addDigit('.')}/>
+					<Button label='=' operation onClick={() => this.setOperation('=')}/>
+					<Button label='%' operation onClick={() => this.setOperation('%')}/>
 				</View>
 			</View>
 		);
