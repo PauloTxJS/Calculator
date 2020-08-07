cb0860a74acfbb926d6f2be8d59b8c49e525c35e
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-08-07 12:40:11 -0300

    Terminei a calculadora

--- App.js
+++ App.js
@@ -17,11 +17,12 @@ export default class App extends Component {
 
 	addDigit = n => {
 
-		if (n === '.' && this.state.displayValue.includes('.')) {
+		const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+		if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
 			return 
 		}
-
-		const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+		
 		const currentValue = clearDisplay ? '' : this.state.displayValue
 		const displayValue = currentValue + n
 		this.setState({ displayValue, clearDisplay: false })
@@ -32,7 +33,7 @@ export default class App extends Component {
 			values[this.state.current] = newValue
 			this.setState({ values }) 	
 		}
-		
+
 	}
 
 	clearMemory = () => {
@@ -40,7 +41,29 @@ export default class App extends Component {
 	}
 
 	setOperation = operation => {
+		
+		if (this.state.current === 0) {
+			this.setState({ operation, current: 1, clearDisplay: true })
+		} else {
+			const equals = operation === '='
+			const values = [...this.state.values]
+			try {
+				values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+			} catch(e) {
+				values[0] = this.state.values[0]
+			}
 
+			values[1] = 0
+			this.setState({
+				displayValue: `${values[0]}`,
+				operation: equals ? null : operation,
+				current: equals ? 0 : 1,
+				//clearDisplay: !equals,
+				clearDisplay: true,	//Reset the display after operation
+				values,	
+			})
+
+		}
 	}
 
 	render() {
@@ -53,7 +76,7 @@ export default class App extends Component {
 					<Button label='7' onClick={() => this.addDigit(7)}/>
 					<Button label='8' onClick={() => this.addDigit(8)}/>
 					<Button label='9' onClick={() => this.addDigit(9)}/>
-					<Button label='x' operation onCLick={() => this.setOperation('*')}/>
+					<Button label='x' operation onClick={() => this.setOperation('*')}/>
 					<Button label='4' onClick={() => this.addDigit(4)}/>
 					<Button label='5' onClick={() => this.addDigit(5)}/>
 					<Button label='6' onClick={() => this.addDigit(6)}/>
